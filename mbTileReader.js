198bee7abb29952dd15a209392f14709a6b4c664
jarle pedersen <jarle.pedersen@norconsult.com>
2018-09-14 13:50:01 +0200

    added metadata

--- mbTileReader.js
+++ mbTileReader.js
@@ -1,10 +1,10 @@
 const sqlite3 = require("sqlite3"); //.verbose();
 
-function readTile(kode, zoom, column, row) {
+function readTile(file, zoom, column, row) {
   return new Promise((resolve, reject) => {
     const sql =
       "SELECT tile_data from tiles WHERE zoom_level=? AND tile_column=? AND tile_row=?";
-    let db = new sqlite3.Database("./" + kode + ".mbtiles");
+    let db = createDb(file);
     db.get(sql, [zoom, column, row], (err, record) => {
       if (err) return reject(err);
       resolve(record ? record.tile_data : null);
@@ -13,4 +13,22 @@ function readTile(kode, zoom, column, row) {
   });
 }
 
-module.exports = { readTile };
+function readMetadata(file){
+  return new Promise((resolve, reject) => {
+  const sql =
+      "SELECT name, value from metadata";
+  let db = createDb(file);
+  db.all(sql, (err, records) => {
+    if (err) return reject(err);
+    resolve(records ? records : null);
+    db.close();
+  });
+});
+}
+
+function createDb(file){
+  return new sqlite3.Database("./" + file + ".mbtiles");
+}
+
+
+module.exports = { readTile, readMetadata };
--- mbtile_routes.js
+++ mbtile_routes.js
@@ -1,4 +1,4 @@
-const { readTile } = require("./mbTileReader");
+const { readTile, readMetadata } = require("./mbTileReader");
 const log = require("log-less-fancy")();
 module.exports = function(app) {
   app.use((req, res, next) => {
@@ -14,7 +14,16 @@ module.exports = function(app) {
     const parts = req.url.split("/");
     const [empty, kode, z, y, x] = parts;
     log.info({ kode, z, y, x });
-    readTile(kode, z, y, x)
+
+    if(kode && !z) { readMetadata(kode)
+      .then(metadata => {
+      if (metadata) res.end(metadata.name);
+      else res.sendFile(__dirname + "/png-transparent.png");
+    })
+    .catch(next);
+  }
+
+    else readTile(kode, z, y, x)
       .then(blob => {
         if (blob) res.end(new Buffer(blob, "binary"));
         else res.sendFile(__dirname + "/png-transparent.png");

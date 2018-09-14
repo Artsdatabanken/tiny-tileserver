11d200a2d9492d407366ec3d58fd29c12168a226
jarle pedersen <jarle.pedersen@norconsult.com>
2018-09-14 16:05:23 +0200

    remove comment

--- mbtile_routes.js
+++ mbtile_routes.js
@@ -3,7 +3,6 @@ const { createMetadata } = require("./metadata");
 const log = require("log-less-fancy")();
 module.exports = function(app) {
   app.use((req, res, next) => {
-    //    res.setHeader("Content-Type", "application/json");
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
       "Access-Control-Allow-Headers",

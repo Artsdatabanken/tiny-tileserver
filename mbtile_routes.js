d349403aed377a7861bc4c26f577432ffde73258
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2018-09-20 09:15:06 +0200

    Fix formatting

--- mbtile_routes.js
+++ mbtile_routes.js
@@ -1,42 +1,40 @@
-const { readTile, readMetadata } = require("./mbTileReader");
-const { createMetadata } = require("./metadata");
-const log = require("log-less-fancy")();
+const { readTile, readMetadata } = require("./mbTileReader")
+const { createMetadata } = require("./metadata")
+const log = require("log-less-fancy")()
+
 module.exports = function(app) {
   app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "*")
     res.setHeader(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
+    )
+    next()
+  })
   app.get("*", (req, res, next) => {
-    const parts = req.url.split("/");
-    const [empty, kode, z, x, y] = parts;
-    log.info({ kode, z, x, y });
+    const parts = req.url.split("/")
+    const [empty, kode, z, x, y] = parts
+    log.info({ kode, z, x, y })
 
-    if(kode && !z) { readMetadata(kode)
-      .then(metadata => {
-      if (metadata) {
-        res.setHeader("Content-Type", "application/json")
-        res.end(createMetadata(metadata));
-      }
-      else res.sendFile(__dirname + "/png-transparent.png");
-    })
-    .catch(next);
-    }
-
-    else readTile(kode, z, x, y)
-      .then(blob => {
-        if (blob) {
-          res.setHeader("Content-Encoding", "gzip")
-          res.setHeader("Content-Type", "application/x-protobuf")
-          res.end(new Buffer(blob, "binary"));
-        }
-        else res.status(404)
-          .send('Not found');
-        // res.sendFile(__dirname + "/png-transparent.png");
-      })
-      .catch(next);
-  });
-};
+    if (kode && !z) {
+      readMetadata(kode)
+        .then(metadata => {
+          if (metadata) {
+            res.setHeader("Content-Type", "application/json")
+            res.end(createMetadata(metadata))
+          } else res.sendFile(__dirname + "/png-transparent.png")
+        })
+        .catch(next)
+    } else
+      readTile(kode, z, x, y)
+        .then(blob => {
+          if (blob) {
+            res.setHeader("Content-Encoding", "gzip")
+            res.setHeader("Content-Type", "application/x-protobuf")
+            res.end(new Buffer(blob, "binary"))
+          } else res.status(404).send("Not found")
+          // res.sendFile(__dirname + "/png-transparent.png");
+        })
+        .catch(next)
+  })
+}

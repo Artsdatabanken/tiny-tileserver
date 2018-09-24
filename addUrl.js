8b32acc76b6df65d8fd4879833eec8c17a082f21
jarle pedersen <jarle.pedersen@norconsult.com>
2018-09-24 12:37:46 +0200

    use forwarded protocol

--- addUrl.js
+++ addUrl.js
@@ -2,7 +2,7 @@ function addUrl(index, req) {
     var keys = Object.keys(index)
     for (key in keys){
         var fileName = keys[key]
-        index[fileName].url = req.protocol + '://' + req.get('host') + req.originalUrl + `${fileName}/{z}/{x}/{y}`
+        index[fileName].url = req.headers['x-forwarded-proto'] + '://' + req.get('host') + req.originalUrl + `${fileName}/{z}/{x}/{y}`
     }
     return index
 }

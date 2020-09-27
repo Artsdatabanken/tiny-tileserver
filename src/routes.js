c791c6e9a870349b9cc4a6b04a9cd752191bb303
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2020-09-27 19:11:14 +0200

    Add request arguemtn 'nohttp204' for leaflet compatibility
    When nohttp204 is present in the query string the normal behaviour of returning 204 for a no data tile changes to returning 200 and a transparent 1px image.
    
    Necessary because Leaflet reports errors when receiving HTTP 204. (tileerror)

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,22 @@
 const { browse } = require("./html");
 const { getCompression } = require("./fileformat/mbtiles/pbf/protobuf");
 
-module.exports = function(app, index) {
+module.exports = function (app, index) {
   app.get("*?", (req, res, next) => {
     index
       .get(decodeURIComponent(req.path), req.query, req.headers.host)
-      .then(node => {
+      .then((node) => {
         if (!node) return next();
         if (node.canBrowse) browse(node, req.path);
-        if (node.contentType === "empty")
-          return res.status(204).send("No Content");
+        if (node.contentType === "empty") {
+          if ("nohttp204" in req.query) {
+            // Hack for Leaflet as it reports errors on HTTP 204 response
+            node.contentType = "image/png";
+            const emptyPng =
+              "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg==";
+            node.buffer = Buffer.from(emptyPng, "base64");
+          } else return res.status(204).send("No Content");
+        }
         if (!node.contentType) return next();
         res.setHeader("Content-Type", node.contentType);
         if (!node.buffer) return res.sendFile(node.physicalDir);
@@ -18,7 +25,7 @@ module.exports = function(app, index) {
         if (compression) res.setHeader("Content-Encoding", compression);
         res.send(node.buffer);
       })
-      .catch(err => {
+      .catch((err) => {
         next(err);
       });
   });
